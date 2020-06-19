38700053a0e85a5e386832aa91ac4503699fc057
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-06-19 07:04:55 +0530

    simple working servlet implementation doGet() and doPost()

--- 1001_Servlet_1/src/com/servlet/AddServlet.java
+++ 1001_Servlet_1/src/com/servlet/AddServlet.java
@@ -21,4 +21,36 @@ public class AddServlet extends HttpServlet
         	   
         	   out.println("Addition is : "+k);    //printing on webpage 
         }
+        
+        
+        //deGet() method will only work with method type get() on a client file
+        /*
+         public void doPost(HttpServletRequest req,HttpServletResponse res) throws  ServletException,IOException,NumberFormatException
+        {
+        	   int i = Integer.parseInt(req.getParameter("num1"));
+        	   int j = Integer.parseInt(req.getParameter("num2"));
+        	   
+        	   int k = i+j;
+        	   
+        	   PrintWriter out = res.getWriter();  //getting writer object 'out' to write on webpage
+        	   
+        	   out.println("Addition is : "+k);    //printing on webpage 
+        }
+        
+         */
+        
+       //dePost() method will only work with method type post() on a client file
+        /*public void doGet(HttpServletRequest req,HttpServletResponse res) throws  ServletException,IOException,NumberFormatException
+        {
+        	   int i = Integer.parseInt(req.getParameter("num1"));
+        	   int j = Integer.parseInt(req.getParameter("num2"));
+        	   
+        	   int k = i+j;
+        	   
+        	   PrintWriter out = res.getWriter();  //getting writer object 'out' to write on webpage
+        	   
+        	   out.println("Addition is : "+k);    //printing on webpage 
+        }
+        
+        */
 }
