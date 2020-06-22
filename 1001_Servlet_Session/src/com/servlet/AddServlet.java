f341e4b36fd9b2a0165a63c86a94bdd0bc535ee9
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-06-22 14:56:52 +0530

    Servlet using HttpSession

--- 1001_Servlet_Session/src/com/servlet/AddServlet.java
+++ 1001_Servlet_Session/src/com/servlet/AddServlet.java
@@ -1,7 +1,26 @@
 package com.servlet;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
 
-public class AddServlet extends HttpServlet {
-
+public class AddServlet extends HttpServlet 
+{
+      public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException
+      {
+    	     int i = Integer.parseInt(req.getParameter("num1"));
+    	     int j = Integer.parseInt(req.getParameter("num2"));
+    	     
+    	     int k = i+j;
+    	     
+    	     HttpSession session = req.getSession();   	     
+    	     session.setAttribute("k",k);
+    	     
+    	     res.sendRedirect("sqrtRequest");
+    	     
+      }
 }
--- 1001_Servlet_Session/src/com/servlet/SquareServlet.java
+++ 1001_Servlet_Session/src/com/servlet/SquareServlet.java
@@ -1,7 +1,24 @@
 package com.servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-public class SquareServlet extends HttpServlet {
-
+public class SquareServlet extends HttpServlet 
+{
+     public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException
+     {     
+    	   HttpSession session = req.getSession();
+    	   int k = (int)session.getAttribute("k");
+    	   
+    	   k = k*k;
+    	   
+    	   PrintWriter out = res.getWriter();
+    	   out.println("Add Square is : "+k);
+    	   
+     }
 }
