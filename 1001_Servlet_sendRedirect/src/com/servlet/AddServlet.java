234b019a4df6f947fbc00bfacb1f1fe9392099ed
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-06-22 11:24:22 +0530

    RequsetDispatcher servlet with sendRedirect()

---
+++ 1001_Servlet_sendRedirect/src/com/servlet/AddServlet.java
@@ -0,0 +1,25 @@
+package com.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet 
+{
+	  public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException
+	  {
+		  int i = Integer.parseInt(req.getParameter("num1"));
+		  int j = Integer.parseInt(req.getParameter("num2"));
+		  
+		  int k = i+j;
+		  
+		  //req.setAttribute("k",k);
+		  
+		  res.sendRedirect("sqrtRequest?k="+k);
+		  
+		  
+	  }
+
+}
