b77233ae7c586fde3bfa04b6c8307e1eea793abf
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-06-23 15:46:33 +0530

    Servlet Context and Config implementation

--- 1001_Servlet_ContextConfig/src/com/servlet/MyServlet2.java
+++ 1001_Servlet_ContextConfig/src/com/servlet/MyServlet2.java
@@ -13,13 +13,14 @@ public class MyServlet2 extends HttpServlet
 {
 	   public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException
 	   {   	
+		   String name = req.getParameter("name");	//Getting parameter from client file	
 		   
            ServletContext con = getServletContext();		   
 		   String tech = con.getInitParameter("tech");
 		   
 		   PrintWriter out = res.getWriter();
 		   
-		   out.println("Technology for Servlet2 is : "+tech);		   
+		   out.println("Technology for "+name+ " Servlet2 is --> "+tech);		   
 		   
 	   }
 
