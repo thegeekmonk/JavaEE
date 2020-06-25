1225ae3b2506d80a15f4dae3d3028201bd2775ba
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-06-25 15:58:33 +0530

    using @Annotation

---
+++ 1003_Hibernate_Annotation/src/com/hibernate/Application.java
@@ -0,0 +1,35 @@
+package com.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class Application
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		Friend fr = new Friend();
+		fr.setId(101);
+		fr.setAge(27);
+		fr.setName("Akhilesh Kumar");
+		
+		Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Friend.class);		
+		ServiceRegistry rg = new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
+		SessionFactory sf = con.buildSessionFactory(rg);		
+		Session session = sf.openSession();
+
+		
+		Transaction tx = session.beginTransaction();
+		session.save(fr);
+		
+		tx.commit();		
+		
+	}
+
+}
---
+++ 1003_Hibernate_Annotation/src/com/hibernate/Friend.java
@@ -0,0 +1,55 @@
+package com.hibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.*;
+
+//import org.hibernate.annotations.Table;
+
+@Entity
+@Table(name = "Dost")
+public class Friend 
+{    
+	 @Id
+     private int id;
+     
+     @Column(name="umar")
+     private int age;
+     
+     private String name;
+     
+     
+	public int getId() 
+	{
+		return id;
+	}
+	
+	public void setId(int id) 
+	{
+		this.id = id;
+	}
+	
+	public int getAge() 
+	{
+		return age;
+	}
+	
+	public void setAge(int age) 
+	{
+		this.age = age;
+	}
+	
+	public String getName() 
+	{
+		return name;
+	}
+	
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+     
+     
+     
+}
