43205f33db15085442dec51a278e99fb9986d98c
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-06-25 17:15:06 +0530

    fetching from database using get()

---
+++ 1003_Hibernate_fetch/src/com/hibernate/Application.java
@@ -0,0 +1,32 @@
+package com.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class Application 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+         
+		Friend fr = null;
+		
+		Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Friend.class);
+		ServiceRegistry rg = new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
+		
+		SessionFactory sf = con.buildSessionFactory(rg);		
+		Session session = sf.openSession();
+	    
+		Transaction tx = session.beginTransaction();
+		
+		fr = session.get(Friend.class,102);
+		
+		System.out.println(fr);
+	}
+
+}
