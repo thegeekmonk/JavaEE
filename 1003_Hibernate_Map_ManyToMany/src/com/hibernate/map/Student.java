19ef0ab022263b3639536bf8e3073f2433010315
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-06-26 23:55:06 +0530

    coding under process @ManyToMany

--- 1003_Hibernate_Map_ManyToMany/src/com/hibernate/map/Student.java
+++ 1003_Hibernate_Map_ManyToMany/src/com/hibernate/map/Student.java
@@ -15,6 +15,33 @@ public class Student
      private int roll;
      private String name;
      
-     @ManyToMany
+     @ManyToMany()
      private ArrayList<Laptop> laptop = new ArrayList<Laptop>();
+
+	public int getRoll() {
+		return roll;
+	}
+
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public ArrayList<Laptop> getLaptop() {
+		return laptop;
+	}
+
+	public void setLaptop(ArrayList<Laptop> laptop) {
+		this.laptop = laptop;
+	}
+     
+     
+     
 }
