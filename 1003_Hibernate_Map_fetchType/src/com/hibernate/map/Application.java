4471a300de63a4b1eac620e7baed6398a7ac6593
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-06-27 13:45:46 +0530

    Coding for fetchType EAGER or LAZY

---
+++ 1003_Hibernate_Map_fetchType/src/com/hibernate/map/Application.java
@@ -0,0 +1,55 @@
+package com.hibernate.map;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class Application 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+//		Student st = new Student();
+//		st.setRoll(10);
+//		st.setName("Akhilesh Kumar");
+//		
+//		Laptop lap1 = new Laptop();
+//		lap1.setLid(1);
+//		lap1.setLname("Dell");
+//		lap1.setStud(st);
+//		
+//		Laptop lap2 = new Laptop();
+//		lap2.setLid(2);
+//		lap2.setLname("HP");
+//		lap2.setStud(st);
+				
+		
+//		Student st2 = new Student();
+//		st2.setRoll(11);
+//		st2.setName("Satya Prasad");
+				
+		
+		Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class).addAnnotatedClass(Laptop.class);
+		ServiceRegistry rg = new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
+		
+		SessionFactory sf = con.buildSessionFactory(rg);
+		Session session = sf.openSession();
+		
+		Transaction tx = session.beginTransaction();
+//		
+//		session.save(st);
+//		
+//		session.save(lap1);
+//		session.save(lap2);
+		
+		tx.commit();
+			
+         
+	}
+
+}
---
+++ 1003_Hibernate_Map_fetchType/src/com/hibernate/map/Laptop.java
@@ -0,0 +1,50 @@
+package com.hibernate.map;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.*;
+
+@Entity
+@Table(name="laptop")
+public class Laptop 
+{   
+	@Id
+    private int lid;
+    private String lname;
+    
+    @ManyToOne
+    private Student stud;
+    
+    
+	public int getLid() 
+	{
+		return lid;
+	}
+	
+	public void setLid(int lid)
+	{
+		this.lid = lid;
+	}
+	
+	public String getLname() 
+	{
+		return lname;
+	}
+	
+	public void setLname(String lname) 
+	{
+		this.lname = lname;
+	}
+
+	public Student getStud() {
+		return stud;
+	}
+
+	public void setStud(Student stud) {
+		this.stud = stud;
+	}
+    
+    
+    
+}
