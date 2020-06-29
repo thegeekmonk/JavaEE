39ef916841b9a26e6cd43633932c5bb7fde52e75
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-06-29 21:33:24 +0530

    Student class coded

---
+++ 1003_Hibernate_Lifecycle/src/com/hibernate/Student.java
@@ -0,0 +1,35 @@
+package com.hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.*;
+
+@Entity
+@Table(name="student")
+public class Student 
+{    
+	 @Id
+     private int roll;
+     private String name;
+     
+     
+	public int getRoll() {
+		return roll;
+	}
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Student [roll=" + roll + ", name=" + name + "]";
+	}
+     
+     
+     
+     
+}
