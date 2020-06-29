12651db5dba7eaadfe8ae4888606a79fd61e731a
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-06-29 19:04:39 +0530

    Adding student entry

--- 1003_Hibernate_GetVsLoad/src/com/hibernate/Application.java
+++ 1003_Hibernate_GetVsLoad/src/com/hibernate/Application.java
@@ -1,5 +1,12 @@
 package com.hibernate;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 public class Application
 {
 
@@ -7,8 +14,21 @@ public class Application
 	{
 		// TODO Auto-generated method stub
 		
+		Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class);		
+		ServiceRegistry rg = new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
 		
+		SessionFactory sf = con.buildSessionFactory(rg);
+		Session session = sf.openSession();
 		
+		Student st = new Student();
+		st.setRoll(11);
+		st.setName("Kushal Jain");
+		
+		Transaction tx = session.beginTransaction();
+		
+		session.save(st);
+		
+		tx.commit();
 
 	}
 
