f42f27dfd6f11014821bd4addf0be1156b4dd1d8
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-06-29 21:38:14 +0530

    hibernate configuration done

--- 1003_Hibernate_Lifecycle/src/com/hibernate/Application.java
+++ 1003_Hibernate_Lifecycle/src/com/hibernate/Application.java
@@ -1,12 +1,15 @@
 package com.hibernate;
 
+import org.hibernate.cfg.Configuration;
+
 public class Application 
 {
 
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-
+       
+		Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class);
 	}
 
 }
