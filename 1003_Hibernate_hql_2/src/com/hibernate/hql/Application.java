33584dc8c8934fb2acd5163733ba6318f4ef16c7
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-06-30 14:56:38 +0530

    started a project

---
+++ 1003_Hibernate_hql_2/src/com/hibernate/hql/Application.java
@@ -0,0 +1,10 @@
+package com.hibernate.hql;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 1003_Hibernate_hql_2/src/com/hibernate/hql/Friend.java
@@ -0,0 +1,5 @@
+package com.hibernate.hql;
+
+public class Friend {
+
+}
