15b40e9d9668869da5678986057f61b54e3526b7
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-06-30 13:40:02 +0530

    Application file coded, Inserting values in friend table for testing

--- 1003_Hibernate_hql_1/src/com/hibernate/Application.java
+++ 1003_Hibernate_hql_1/src/com/hibernate/Application.java
@@ -1,5 +1,14 @@
 package com.hibernate;
 
+import java.util.Random;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 public class Application 
 {
 
@@ -7,8 +16,27 @@ public class Application
 	{
 		// TODO Auto-generated method stub
 		
+		Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Friend.class);
+		ServiceRegistry rg = new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
+		SessionFactory sf = con.buildSessionFactory(rg);
 		
+		Session session = sf.openSession();
 		
+		Transaction tx = session.beginTransaction();
+		
+		Random r = new Random();
+		
+		for(int i = 1;i <= 20;i++)
+		{
+			Friend fr = new Friend();
+			fr.setId(i);
+			fr.setName("Name --> "+i);
+			fr.setAge(r.nextInt(70));
+			
+			session.save(fr);
+		}		
+		
+		tx.commit();
 		
 
 	}
