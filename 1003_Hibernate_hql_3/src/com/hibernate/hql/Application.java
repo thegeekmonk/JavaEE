c53df1314c8075ce2c3d1ae0f164c521169c8cad
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-06-30 19:06:38 +0530

    Application class coding done

--- 1003_Hibernate_hql_3/src/com/hibernate/hql/Application.java
+++ 1003_Hibernate_hql_3/src/com/hibernate/hql/Application.java
@@ -1,6 +1,11 @@
 package com.hibernate.hql;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 public class Application 
 {
@@ -10,6 +15,15 @@ public class Application
 		// TODO Auto-generated method stub
         
 		Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Friend.class);
+		ServiceRegistry rg = new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
+		SessionFactory sf = con.buildSessionFactory(rg);
+		
+		Session session = sf.openSession();
+		
+		Transaction tx = session.beginTransaction();
+		
+		
+		tx.commit();
 		
 	}
 
