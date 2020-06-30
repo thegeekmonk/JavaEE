2e450c39a8d253cfb2a3c0351f717c7bdb8b073a
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-06-30 12:09:37 +0530

    started a project

---
+++ 1003_Hibernate_JPA/src/com/hibernate/jpa/Application.java
@@ -0,0 +1,14 @@
+package com.hibernate.jpa;
+
+public class Application 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello JPA");
+		
+	}
+
+}
