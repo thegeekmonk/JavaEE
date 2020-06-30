7527630e5310643690605e92c8c5df8d93a9eab3
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-06-30 18:57:48 +0530

    hibernate configuration created

--- 1003_Hibernate_hql_3/src/com/hibernate/hql/Application.java
+++ 1003_Hibernate_hql_3/src/com/hibernate/hql/Application.java
@@ -1,12 +1,16 @@
 package com.hibernate.hql;
 
+import org.hibernate.cfg.Configuration;
+
 public class Application 
 {
 
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-
+        
+		Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Friend.class);
+		
 	}
 
 }
