2f9b8ebd126e08748b9a9fa1f6cb4d772d99d46a
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-06-30 16:34:26 +0530

    Fetching particular column from table using HQL

--- 1003_Hibernate_hql_2/src/com/hibernate/hql/Application.java
+++ 1003_Hibernate_hql_2/src/com/hibernate/hql/Application.java
@@ -1,5 +1,8 @@
 package com.hibernate.hql;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -20,10 +23,16 @@ public class Application
 		
 		Session session = sf.openSession();
 		
+		Query q = session.createQuery("select id,name from Friend");
 		
+		List<Object[]> friend = (List<Object[]>)q.list();
 		
 		Transaction tx = session.beginTransaction();
 		
+		for(Object[] fr : friend)
+		{
+			System.out.println(fr[0]+" : "+fr[1]);
+		}
 		
 		
 		tx.commit();
