0e48e7afaa5205f6e4f058183a8a31ef3a9af804
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-07-02 16:17:43 +0530

    Stdent class coded

--- 1003_Hibernate_Cache_L1/src/com/hibernate/cache/Student.java
+++ 1003_Hibernate_Cache_L1/src/com/hibernate/cache/Student.java
@@ -1,6 +1,41 @@
 package com.hibernate.cache;
 
-public class Student 
-{
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.*;
 
+
+@Entity
+@Table(name="student")
+public class Student 
+{     
+	  @Id
+      private int id;
+      private String name;     
+      
+	public int getId() 
+	{
+		return id;
+	}
+	public void setId(int id) 
+	{
+		this.id = id;
+	}
+	public String getName() 
+	{
+		return name;
+	}
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + "]";
+	} 
+	
+	
+      
 }
