a06dfdfc81ca72aacff92160aba92f1040632e99
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-07-04 14:30:58 +0530

    Entity class coded

---
+++ 1003_Hibernate_Cache_L2_hql/src/com/hibernate/cache/Student.java
@@ -0,0 +1,43 @@
+package com.hibernate.cache;
+
+import javax.persistence.Cacheable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;  
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+@Entity
+@Table(name = "student")
+@Cacheable
+@Cache(usage=CacheConcurrencyStrategy.READ_ONLY)
+public class Student 
+{      
+	   @Id
+       private int roll;
+       private String name;
+       
+       
+	public int getRoll() {
+		return roll;
+	}
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Student [roll=" + roll + ", name=" + name + "]";
+	}
+       
+       
+       
+}
