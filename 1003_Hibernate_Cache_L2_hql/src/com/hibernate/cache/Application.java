e20668ebb20e70da1795fd81d0b0c4315fd44d8d
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-07-05 11:48:11 +0530

    Application class updated

--- 1003_Hibernate_Cache_L2_hql/src/com/hibernate/cache/Application.java
+++ 1003_Hibernate_Cache_L2_hql/src/com/hibernate/cache/Application.java
@@ -1,12 +1,32 @@
 package com.hibernate.cache;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 public class Application 
 {
     
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-
+       
+		Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class);
+		ServiceRegistry rg = new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
+		
+		SessionFactory sf = con.buildSessionFactory(rg);
+		Session session = sf.openSession();
+		
+		Transaction tx = session.beginTransaction();
+		
+		
+		
+		
+		
+		tx.commit();
 	}
 
 }
--- 1003_Hibernate_Cache_L2_hql/src/com/hibernate/cache/Student.java
+++ 1003_Hibernate_Cache_L2_hql/src/com/hibernate/cache/Student.java
@@ -19,25 +19,31 @@ public class Student
        private String name;
        
        
-	public int getRoll() {
+	public int getRoll() 
+	{
 		return roll;
 	}
-	public void setRoll(int roll) {
+	
+	public void setRoll(int roll)
+	{
 		this.roll = roll;
 	}
-	public String getName() {
+	
+	public String getName()
+	{
 		return name;
 	}
-	public void setName(String name) {
+	
+	public void setName(String name) 
+	{
 		this.name = name;
 	}
 	
 	
 	@Override
-	public String toString() {
+	public String toString() 
+	{
 		return "Student [roll=" + roll + ", name=" + name + "]";
-	}
-       
-       
+	}       
        
 }
