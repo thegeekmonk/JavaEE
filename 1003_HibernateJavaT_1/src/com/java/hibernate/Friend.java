0a0c58a0aeaf73bcf61a206c95534e3d40f371c1
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-08-16 10:13:34 +0530

    persistent class getter and setter

--- 1003_HibernateJavaT_1/src/com/java/hibernate/Friend.java
+++ 1003_HibernateJavaT_1/src/com/java/hibernate/Friend.java
@@ -7,4 +7,25 @@ public class Friend
       private int age;
       
       
+      
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+      
+      
 }
