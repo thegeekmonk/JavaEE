d7888e6d3227073a5b767ae75d06c981b235a9ab
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-08-17 15:20:10 +0530

    mapping file updated

--- 1003_HibernateJavaT_1/src/com/java/hibernate/Friend.java
+++ 1003_HibernateJavaT_1/src/com/java/hibernate/Friend.java
@@ -8,25 +8,36 @@ public class Friend
       private int id;
       private String name;
       private int age;
+           
       
-      
-      
-	public int getId() {
+	public int getId() 
+	{	
 		return id;
 	}
-	public void setId(int id) {
+	
+	public void setId(int id) 
+	{
 		this.id = id;
 	}
-	public String getName() {
+	
+	public String getName() 
+	{
 		return name;
 	}
-	public void setName(String name) {
+	
+	public void setName(String name) 
+	{
 		this.name = name;
 	}
-	public int getAge() {
+	
+	public int getAge() 
+	{
 		return age;
 	}
-	public void setAge(int age) {
+	
+	
+	public void setAge(int age) 
+	{
 		this.age = age;
 	}
       
