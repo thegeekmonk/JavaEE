d6567bc1fee52e6b4c948da83ad24ef819d24a4b
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-08-17 15:34:32 +0530

    Application class updated

--- 1003_HibernateJavaT_1/src/com/java/hibernate/Application.java
+++ 1003_HibernateJavaT_1/src/com/java/hibernate/Application.java
@@ -1,5 +1,13 @@
 package com.java.hibernate;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class Application 
 {
 
@@ -7,6 +15,28 @@ public class Application
 	{
 		// TODO Auto-generated method stub
 
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+		
+		Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+		SessionFactory factory = meta.getSessionFactoryBuilder().build();
+		Session session = factory.openSession();
+		
+		Transaction t = session.beginTransaction();
+		
+		
+		Friend fr = new Friend();
+		fr.setId(101);
+		fr.setAge(27);
+		fr.setName("August");
+		
+		session.save(fr);
+		t.commit();
+		
+		System.out.println("Row added successfully");
+		session.close();
+		factory.close();
+		
+		
 	}
 
 }
