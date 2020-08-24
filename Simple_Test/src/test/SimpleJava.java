36128c0eed28faa8d0d52375046229d493edae7e
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-08-25 00:31:36 +0530

    Simple java program

---
+++ Simple_Test/src/test/SimpleJava.java
@@ -0,0 +1,21 @@
+package test;
+
+public class SimpleJava 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		String str = "hello world hello people java python";
+		
+		String arr[] = str.split("/s");
+		
+		for(String a : arr)
+		  System.out.println(a);
+		
+		
+
+	}
+
+}
