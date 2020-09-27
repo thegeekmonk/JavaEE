4accd2f0454d5be5468b6c95620e6bf6fb3dbe3c
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-09-27 18:18:25 +0530

    Simple test

--- Simple_Test/src/test/SimpleJava.java
+++ Simple_Test/src/test/SimpleJava.java
@@ -7,15 +7,19 @@ public class SimpleJava
 	{
 		// TODO Auto-generated method stub
 		
-		String str = "hello world hello people java python";
+//		String str = "hello world hello people java python";
+//		
+//		String arr[] = str.split("/s");
 		
-		String arr[] = str.split("/s");
+//		for(String a : arr)
+//		  System.out.println(a);
 		
-		for(String a : arr)
-		  System.out.println(a);
+		int arr[] = {2,-3};
 		
+		int sum = arr[0] - arr[1];
 		
-
+		System.out.println(sum);
+	
 	}
 
 }
