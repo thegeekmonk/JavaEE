b30d4b3dc53c8fc29c52c0f739d37510770e6a4d
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-03-17 09:43:15 +0530

    @Object Pool design pattern

---
+++ Java_Design_Pattern/src/pattern/objectpool/ExportingTask.java
@@ -0,0 +1,26 @@
+package pattern.objectpool;
+
+public class ExportingTask implements Runnable{
+    
+	private ObjectPool<ExportingProcess> pool;
+	private int threadNo;
+	
+	public ExportingTask(int threadNo, ObjectPool<ExportingProcess> pool)
+	{
+		this.threadNo = threadNo;
+		this.pool = pool;
+	}
+	
+	public void run()
+	{
+		
+		ExportingProcess exportingProcess = pool.borrowObject();
+		
+		System.out.println("Thread : "+threadNo+" Object with processNo "+exportingProcess.getProcessNo()+" Was borrowed");
+		
+		
+		pool.returnObject(exportingProcess);
+		
+		System.out.println("Thread : "+threadNo+" Object with processNo "+exportingProcess.getProcessNo()+" Was returned");
+	}
+}
