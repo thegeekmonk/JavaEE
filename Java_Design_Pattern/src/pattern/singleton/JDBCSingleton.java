2fd4180dc7554ea0da0f9391c03ae0ae47d5bc73
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-04-15 09:30:07 +0530

    @Singleton Pattern

--- Java_Design_Pattern/src/pattern/singleton/JDBCSingleton.java
+++ Java_Design_Pattern/src/pattern/singleton/JDBCSingleton.java
@@ -23,9 +23,9 @@ public class JDBCSingleton {
 	
 	private static Connection getConnection() throws ClassNotFoundException,SQLException
 	{
-		Connection con;        
-		Class.forName("oracle.jdbc.driver.OracleDriver");		
-		con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:oracle","sys as sysdba","5441");
+		
+		Class.forName("oracle.jdbc.driver.OracleDriver");			
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:sys as sysdba/5441@localhost:1521:oracle");
 				
 	  return con;	
 	}	
