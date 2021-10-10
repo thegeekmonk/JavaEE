1a08fc3e6ab7fadd7c7a79180625a5d8b8596fb2
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-10-10 17:40:05 +0530

    @Caching 2nd level with @Query

--- Hibernate/src/main/java/com/java/hibernate/cache/CacheAppl.java
+++ Hibernate/src/main/java/com/java/hibernate/cache/CacheAppl.java
@@ -1,9 +1,12 @@
 package com.java.hibernate.cache;
 
+
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 import org.hibernate.service.ServiceRegistry;
 
 public class CacheAppl {
@@ -22,8 +25,14 @@ public class CacheAppl {
 		
 		session.beginTransaction();
 		
-		emp = session.get(Employee.class, 101);		
+//		emp = session.get(Employee.class, 101);		
+//		System.out.println(emp);
+		
+		Query q1 = session.createQuery("from Employee where id=101");
+		q1.setCacheable(true);
+		emp = (Employee)q1.getSingleResult();
 		System.out.println(emp);
+		
 		
 //		emp = session.get(Employee.class, 101);
 //		System.out.println(emp);
@@ -35,9 +44,15 @@ public class CacheAppl {
 		Session session2 = sf.openSession();
 		session2.beginTransaction();	
 		
-		emp = session2.get(Employee.class, 101);		
+		//emp = session2.get(Employee.class, 101);
+				
+		
+		Query q2 = session2.createQuery("from Employee where id=101");
+		q2.setCacheable(true);
+		emp = (Employee)q2.getSingleResult();
 		System.out.println(emp);
 		
+
 		session2.getTransaction().commit();
 		session2.close();
 	}
